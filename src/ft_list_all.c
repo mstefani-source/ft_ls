2485059090045c45d610e425dd0c8faaf5d98b45
mstefani <ivanow.victor@live.ru>
2022-01-15 17:10:05 +0300

    prf

--- src/ft_list_all.c
+++ src/ft_list_all.c
@@ -9,12 +9,10 @@ int		ft_show_dir(char* pathdir)
 	ft_printf("inode\t\t name\t type\t reclen\n");
     while ( (entry = readdir(dir)) != NULL) 
 	{		
-  //      ft_printf("%ld \t - \t %s \t [%d] \t %d\n", entry->d_ino, entry->d_name, entry->d_type, entry->d_reclen);
+        ft_printf("%ld \t - \t %s \t [%d] \t %d\n", entry->d_ino, entry->d_name, entry->d_type, entry->d_reclen);
   		printf("%ld \t - \t %s \t [%d] \t %d\n", entry->d_ino, entry->d_name, entry->d_type, entry->d_reclen);
-
     }
     closedir(dir);
-
 	pathdir++;
 	ft_printf("%s\n", pathdir);
 	return(0);
